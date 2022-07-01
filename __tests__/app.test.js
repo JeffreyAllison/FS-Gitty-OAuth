ab7597839409aff4ae0040ec995f696fbbb131b2
JeffAllison <itsjallison@gmail.com>
2022-07-01 15:47:16 -0700

    altered url for oauth route test, test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,20 +2,21 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+jest.mock('../lib/services/github');
 
 describe('gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('route should redirect to  github oauth page at login', async () => {
-    const res = await request(app).get('/api/v1/github/login');
-    expect(res.header.location).toMatch(
-      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
-    );
-  });
-
   afterAll(() => {
     pool.end();
+  });
+
+  it('route should redirect to github oauth page at login', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(
+      `https://github.com/login/oauth/authorize?client_id=${process.env.GH_CLIENT_ID}&scope=user&redirect_`
+    );
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS github_users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
 
 CREATE TABLE github_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,3 +10,10 @@ CREATE TABLE github_users (
   email TEXT, 
   avatar TEXT
 );
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT, 
+  posts VARCHAR(255),
+  FOREIGN KEY (user_id) REFERENCES github_users(id)
+)
